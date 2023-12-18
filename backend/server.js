a7ce9a6067d1e2496bf827ffe5080401c5c693c0
Nada <nada@nada.com>
2023-12-18 22:58:54 +0100

    v2.0

--- backend/server.js
+++ backend/server.js
@@ -38,20 +38,61 @@ app.post("/signin", (req, res) => {
   });
 });
 
-app.post("/signin", (req, res) => {
-  const sql = "SELECT * FROM `users` WHERE `email` = ? AND `password` = ?";
-  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+app.post("/insert_products", (req, res) => {
+  const sql =
+    "INSERT INTO `produits`(`Nom`, `Prix`, `Description`, `Stock`, `Source`, `Categorie`) VALUES ?";
+
+  const values = req.body;
+  db.query(sql, [values], (err, data) => {
     if (err) {
+      console.log("Error");
       return res.json("Error");
     }
-    if (data.length > 0) {
-      return res.json("Success");
-    } else {
-      return res.json("Fail");
-    }
+    console.log("Success");
+    return res.json("Products inserted successfully");
   });
 });
 
+app.post("/insert_nom", (req, res) => {
+  const sql =
+    "INSERT INTO `nomenclatures`(`Nom`, `Prix`, `Description`, `Stock`, `Source`, `Fournisseur`) VALUES ?";
+
+  const values = req.body;
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json("Nomenclatures inserted successfully");
+  });
+});
+
+app.post("/insert_stock", (req, res) => {
+  const sql =
+    "INSERT INTO `stock`(`Nom`, `Location`, `Product`, `Quantity`) VALUES ?";
+
+  const values = req.body;
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json("Stock info inserted successfully");
+  });
+});
+
+app.post("/insert_oper", (req, res) => {
+  const sql =
+    "INSERT INTO `operations`(`operation`, `quantity`, `duration`) VALUES ?";
+
+  const values = req.body;
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      console.log("Error");
+      return res.json("Error");
+    }
+    console.log("Success");
+    return res.json("Operations inserted successfully");
+  });
+});
 
 app.listen(8001, () => {
   console.log("listening on port 8001");
